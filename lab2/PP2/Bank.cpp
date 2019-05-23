44311feff9e989cd2757d1d737e083febdebb032
Michael <moskvichevmihail60@yandex.ru>
2019-05-23 21:46:57 +0300

    todo in Bank.cpp

--- lab2/PP2/Bank.cpp
+++ lab2/PP2/Bank.cpp
@@ -1,9 +1,18 @@
 #include "Bank.h"
 
-CBank::CBank()
+CBank::CBank(int lockTool)
 {
 	m_clients = std::vector<CBankClient>();
 	m_totalBalance = 0;
+	InitializeCriticalSection(&m_criticalSection);
+	if (lockTool = 0)
+	{
+		m_lock_tool = Mutex;
+	}
+	else
+	{
+		m_lock_tool = CriticalSection;
+	}
 }
 
 
@@ -18,10 +27,12 @@ CBankClient* CBank::CreateClient()
 
 void CBank::UpdateClientBalance(CBankClient &client, int value)
 {
+	ToLockSection();
 	int totalBalance = GetTotalBalance();
 	std::cout << "Client " << client.GetId() << " initiates reading total balance. Total = " << totalBalance << "." << std::endl;
 	
 	SomeLongOperations();
+	SetClientBalance(client, value);
 	totalBalance += value;
 
 	std::cout
@@ -35,12 +46,57 @@ void CBank::UpdateClientBalance(CBankClient &client, int value)
 	}
 
 	SetTotalBalance(totalBalance);
+	ToUnlockSection();
+}
+
+vector<CBankClient> CBank::GetClients()
+{
+	return m_clients;
 }
 
 
 int CBank::GetTotalBalance()
 {
 	return m_totalBalance;
+}
+
+void CBank::SetClientBalance(CBankClient client, int value)
+{
+	auto clientAndBalance = m_clientAndBalance.find(client.GetId());
+
+	if (clientAndBalance != m_clientAndBalance.end())
+	{
+		clientAndBalance->second += value;
+	}
+	else
+	{
+		m_clientAndBalance.emplace(pair<int, int>(client.GetId(), value));
+	}
+}
+
+int CBank::GetClientBalance(CBankClient client)
+{
+	return m_clientAndBalance.at(client.GetId());
+}
+
+void CBank::ToLockSection()
+{
+	if (m_lock_tool == CriticalSection) {
+		EnterCriticalSection(&m_criticalSection);
+	}
+	else if (m_lock_tool == Mutex) {
+		m_mutex.lock();
+	}
+}
+
+void CBank::ToUnlockSection()
+{
+	if (m_lock_tool == CriticalSection) {
+		LeaveCriticalSection(&m_criticalSection);
+	}
+	else if (m_lock_tool == Mutex) {
+		m_mutex.unlock();
+	}
 }
 
 
@@ -52,4 +108,5 @@ void CBank::SetTotalBalance(int value)
 void CBank::SomeLongOperations()
 {
 	// TODO
+	Sleep(1000);
 }
