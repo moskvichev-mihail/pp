b71b4d5b9f6ac0a345844b2c263e847414367c8d
Michael <moskvichevmihail60@yandex.ru>
2019-06-04 22:42:21 +0300

    readd lab3

---
+++ lab3/lab3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 3)
+            {
+                Console.WriteLine("Incorrect number of arguments.");
+                return;
+            }
+        }
+    }
+}
