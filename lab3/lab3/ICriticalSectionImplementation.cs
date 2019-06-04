d57f06b09cf28988ec8339fd01c5f068cc1d28f7
Michael <moskvichevmihail60@yandex.ru>
2019-06-04 22:57:58 +0300

    Create ICriticalSectionImplementation.cs

---
+++ lab3/lab3/ICriticalSectionImplementation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    public class ICriticalSectionImplementation : ICriticalSection
+    {
+        public void Enter() { }
+
+        public void Leave() { }
+
+        public bool TryEnter(int timeout) { }
+
+        public void SetSpinCount(int count) { }
+    }
+}
